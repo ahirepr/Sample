a04d74cdc55e43366dbc72e9fecd9ee8c5d36dcd
pahire <ahirepr2017@gmail.com>
2022-03-01 08:46:36 +0530

    Initial commite via git cmd

---
+++ src/main/java/com/pankajatach/SampleApplication03Application.java
@@ -0,0 +1,13 @@
+package com.pankajatach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleApplication03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleApplication03Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/pankajatach/SampleApplication03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pankajatach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleApplication03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
